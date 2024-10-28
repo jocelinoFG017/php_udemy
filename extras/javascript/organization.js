3a9c7220049f280f0dabb95047906b2e1cc0deb2
jocelino017 <jocelinogg@gmail.com>
2024-10-28 14:16:30 -0300

    fix: new content

---
+++ extras/javascript/organization.js
@@ -0,0 +1,13 @@
+console.log("sentença de código")
+
+// ; é opcional no javascript
+{
+    {
+        console.log("Olá");
+        console.log("Mundo") // padrão do curso
+    }
+}
+
+// Javascript é organizado em sentença de código
+// Com ; sendo opcional
+// E blocos de código.
