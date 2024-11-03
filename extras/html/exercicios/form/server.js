b817346ea19af1c21b4f4542a4a5a0a30baefa39
jocelino017 <jocelinogg@gmail.com>
2024-11-03 11:49:01 -0300

    feat: Finalizando conteudo da seção 22: HTML

--- extras/html/exercicios/form/server.js
+++ extras/html/exercicios/form/server.js
@@ -4,9 +4,17 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+// incluir
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabéns</h1>')
+    // console.log(re.query) // utilizado com app.get
+    resp.send('<h1>Parabéns. Usuário Incluido </h1>')
+})
+// alterar
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Alterado</h1>')
 })
 
 app.listen(3003)
